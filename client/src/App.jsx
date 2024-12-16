b869e4c105fa842dfa9502180474659f22f1c716
palakk2022 <thakur2205palak@gmail.com>
2024-12-16 12:22:51 +0530

    complete listing item and show more functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './Components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
 import UpdateListing from './Pages/UpdateListing';
 import Listing from './Pages/Listing';
+import Search from './Pages/Search';
 
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
       <Route path="/sign-Up" element={ <Signup />}/>
       <Route path="/about" element={<About />}/>
       <Route path="/contact-us" element={<Contactus/>}/>
+      <Route path="/search"element={<Search/>}/>
       <Route path="/listing/:listingId" element={<Listing/>}/>
 
 
